b12c4bb60a7b84b6a87dbe10ac07a9f2c1cd4f06
TomaszLach2 <tomasz.lach@speardevs.com>
2023-06-20 10:48:04 +0200

    Add user groups to form

--- src/Form/Type/Admin/SendPushNotificationType.php
+++ src/Form/Type/Admin/SendPushNotificationType.php
@@ -4,15 +4,21 @@ declare(strict_types=1);
 
 namespace SpearDevs\SyliusPushNotificationsPlugin\Form\Type\Admin;
 
+use Sylius\Component\Customer\Model\CustomerGroup;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SendPushNotificationType extends AbstractType
 {
+
+    public function __construct(private TranslatorInterface $translator) {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -34,6 +40,15 @@ class SendPushNotificationType extends AbstractType
                 'constraints' => [
                     new NotBlank()
                 ]
+            ])
+            ->add('groups', EntityType::class, [
+                'label' => 'speardevs_sylius_push_notification_plugin.ui.send_to',
+                'class' => CustomerGroup::class,
+                'expanded' => true,
+                'required' => false,
+                'placeholder' => $this->translator->trans(
+                    'speardevs_sylius_push_notification_plugin.ui.all', [], 'messages'
+                ),
             ]);
     }
 }
