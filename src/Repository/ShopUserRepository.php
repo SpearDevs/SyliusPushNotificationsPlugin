f20d57b8762e93aac787d1fbf323273ceba2326e
TomaszLach2 <tomasz.lach@speardevs.com>
2023-06-20 10:46:58 +0200

    Extend UserRepository

---
+++ src/Repository/ShopUserRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SpearDevs\SyliusPushNotificationsPlugin\Repository;
+
+use Sylius\Bundle\CoreBundle\Doctrine\ORM\UserRepository;
+
+final class ShopUserRepository extends UserRepository
+{
+    public function findUsersByGroup(?string $groupName): array
+    {
+        $queryBuilder = $this->createQueryBuilder('shopUser')
+            ->select('shopUser');
+
+        if ($groupName != '') {
+            $queryBuilder
+                ->join('shopUser.customer', 'customer')
+                ->join('customer.group', 'g')
+                ->where('g.name = :groupName')
+                ->setParameter('groupName', $groupName);
+        }
+
+        return $queryBuilder
+            ->getQuery()
+            ->getResult();
+    }
+}
