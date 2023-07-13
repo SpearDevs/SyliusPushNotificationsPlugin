300864006ddb19e5a0a53b96b3a933b870bd3b01
TomaszLach2 <tomasz.lach@speardevs.com>
2023-07-13 12:25:00 +0200

    Code review fixes

--- src/WebPush/WebPushException.php
+++ src/WebPush/WebPushException.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace SpearDevs\SyliusPushNotificationsPlugin\WebPush;
 
-use Symfony\Component\HttpFoundation\Response;
-
 final class WebPushException extends \Exception
 {
-    public function __construct(string $message, int $code = Response::HTTP_NOT_FOUND, \Throwable $previous = null)
+    public function __construct(string $message)
     {
-        parent::__construct($message, $code, $previous);
+        parent::__construct($message);
     }
 }
