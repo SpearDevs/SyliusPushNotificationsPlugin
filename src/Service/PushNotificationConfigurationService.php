9cde20c6984fb3bb7a20accab5a7bae1f1ed21d6
TomaszLach2 <tomasz.lach@speardevs.com>
2023-07-14 13:27:12 +0200

    Change of host fetch depending on the channel

--- src/Service/PushNotificationConfigurationService.php
+++ src/Service/PushNotificationConfigurationService.php
@@ -6,12 +6,13 @@ namespace SpearDevs\SyliusPushNotificationsPlugin\Service;
 
 use SpearDevs\SyliusPushNotificationsPlugin\Entity\PushNotificationConfiguration\PushNotificationConfigurationInterface;
 use SpearDevs\SyliusPushNotificationsPlugin\Repository\PushNotificationConfiguration\PushNotificationConfigurationRepositoryInterface;
+use Psr\Container\ContainerInterface;
 
 final class PushNotificationConfigurationService
 {
     public function __construct(
         private PushNotificationConfigurationRepositoryInterface $pushNotificationConfigurationRepository,
-        private string $imageUrl,
+        private ContainerInterface $container,
     ) {
     }
 
@@ -23,7 +24,11 @@ final class PushNotificationConfigurationService
             return null;
         }
 
+        $appScheme = $this->container->getParameter('router.request_context.scheme');
+        $appHost = $this->container->get('sylius.context.channel')->getChannel()->getHostName();
+        $imagesDirectory = $this->container->getParameter('images_directory');
+
         /** @var $pushNotificationConfiguration PushNotificationConfigurationInterface */
-        return $this->imageUrl . $pushNotificationConfiguration->getIconPath();
+        return $appScheme . '://' . $appHost . $imagesDirectory . $pushNotificationConfiguration->getIconPath();
     }
 }
