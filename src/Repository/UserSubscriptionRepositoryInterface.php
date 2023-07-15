78842eda55067f5c093487ad2b31be7f653013eb
TomaszLach2 <tomasz.lach@speardevs.com>
2023-07-15 10:49:44 +0200

    Refactor UserSubscriptionRepositoryInterface

--- src/Repository/UserSubscriptionRepositoryInterface.php
+++ src/Repository/UserSubscriptionRepositoryInterface.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace SpearDevs\SyliusPushNotificationsPlugin\Repository;
 
-use BenTools\WebPushBundle\Model\Subscription\UserSubscriptionInterface;
-
 interface UserSubscriptionRepositoryInterface
 {
     public function getSubscriptionsForAllUsers(): iterable;
@@ -13,8 +11,4 @@ interface UserSubscriptionRepositoryInterface
     public function getSubscriptionsForUsersInGroup(string $groupName): iterable;
 
     public function getSubscriptionsForUserByEmail(string $email): iterable;
-
-    public function save(UserSubscriptionInterface $userSubscription): void;
-
-    public function delete(UserSubscriptionInterface $userSubscription): void;
 }
